6f3aeb7998233e64f97148f8a52fa74f11f9b188
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2025-02-24 11:37:03 +0000

    exercicios feitos

---
+++ ex_aula2/ex03.py
@@ -0,0 +1,22 @@
+estados = ['q1','q2','q3','q4']
+alfabeto = ['0','1']
+inicial = 'q1'
+atual = inicial
+aceitacao = ['q2','q3']
+funcao = {
+    'q1' : {'1' : 'q2', '0' : 'q1'},
+    'q2' : {'1' : 'q3', '0' : 'q3'},
+    'q3' : {'0' : 'q4', '1' : 'q2'},
+    'q4' : {'1' : 'q4', '0' : 'q4'},
+    }
+
+input = input()
+palavras = input.split()
+for palavra in palavras:
+    for caractere in palavra:
+        atual = funcao[atual][caractere]
+    if atual in aceitacao:
+        print("aceito")
+    else:
+        print("rejeito")
+    atual = inicial
---
+++ ex_aula2/ex06.py
@@ -0,0 +1,23 @@
+estados = ['q1','q2','q3','q4','q5']
+alfabeto = ['0','1']
+inicial = 'q1'
+atual = inicial
+aceitacao = ['q4','q3']
+funcao = {
+    'q1' : {'1' : 'q1', '0' : 'q2'},
+    'q2' : {'1' : 'q2', '0' : 'q3'},
+    'q3' : {'0' : 'q3', '1' : 'q4'},
+    'q4' : {'1' : 'q5', '0' : 'q4'},
+    'q5' : {'1' : 'q5', '0' : 'q5'}
+    }
+
+input = input()
+palavras = input.split()
+for palavra in palavras:
+    for caractere in palavra:
+        atual = funcao[atual][caractere]
+    if atual in aceitacao:
+        print("aceito")
+    else:
+        print("rejeito")
+    atual = inicial

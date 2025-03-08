36d119b8d70ea53e77c6ad01ad4807dc5bc7dde2
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2025-03-08 01:11:27 +0000

    a principio works

--- atividade4.py
+++ atividade4.py
@@ -5,7 +5,7 @@ aceitacao = []
 funcao = {
     
 }
-tempfunc = []
+#tempfunc = []
 estados_entrada = input().split()
 for estado in estados_entrada:
     estados.append(estado)
@@ -24,25 +24,30 @@ for aceita in aceitacao_entrada:
 
 for i in range(len(estados)):
     func_entrada = input().split()
-    func = set()
+    func = []
     for f in func_entrada:
         if f == "vazio":
-            func.add("")
-            break
+            func.append("")
+            continue
+        func_temp = []
         for item in f.split(","):
-            func.add(item)
+            func_temp.append(item)
+        func.append(func_temp)
     
-
-    funcao[func[0]] = {alfabeto[0] : func[1], alfabeto[1] : func[2], {""} : func[3]}
-    tempfunc = []
+    funcao[func[0][0]] = {alfabeto[0]: set(func[1]), alfabeto[1]: set(func[2]), "": set(func[3])}
+    #tempfunc = []
 
 input = input()
 palavras = input.split()
 for palavra in palavras:
+    atual = inicial
     for caractere in palavra:
-        atual = funcao[atual][caractere]
+        if caractere in funcao[atual]:
+            atual = next(iter(funcao[atual][caractere]))
+        else:
+            atual = None
+            break
     if atual in aceitacao:
         print("aceita")
     else:
         print("rejeita")
-    atual = inicial
